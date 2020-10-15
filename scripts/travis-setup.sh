a297f3b46209903283d2afcc61cd4ea2d6c47e1e
DikshantJajedi <50725236+DikshantJajedi@users.noreply.github.com>
2020-10-15 06:45:11 +0530

    Update travis-setup.sh

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -14,7 +14,8 @@
 # limitations under the License.
 
 set -ex
-
+arch=$(uname -m)
+echo $arch
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
